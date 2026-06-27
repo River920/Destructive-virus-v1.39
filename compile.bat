e14d18391ce4ecfee740d3a315daf583dadfe320
River Manoukian <river.m.phoenix@gmail.com>
2026-06-28 09:35:47 +1000

    reset project

--- compile.bat
+++
@@ -1,7 +0,0 @@
-call "C:\Users\river\AppData\Local\bin\NASM\nasm.exe" -f bin -o out/overwrite/bin/boot.bin src/overwrite/boot.asm
-
-
-
-
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
-cl src/overwrite/ow.cpp -o out/overwrite/ow.exe
